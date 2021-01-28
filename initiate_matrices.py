7171c4feb5f93e4fc32e60d7af137cd836938948
Rutger van Woerkom <rutgervanwoerkom@gmail.com>
2021-01-28 17:04:07 +0100

    clean sweep + begin aan parameters

--- initiate_matrices.py
+++ initiate_matrices.py
@@ -1,16 +1,16 @@
-#!/usr/bin/env python
+# !/usr/bin/env python
 # coding: utf-8
 
 # In[13]:
 
 import numpy as np
 
-#trust matrix does not change within a game, sus matrix does
-#sus matrix starts with 1 - trust
+# trust matrix does not change within a game, sus matrix does
+# sus matrix starts with 1 - trust
 
 
-#initiate matrixes should be run 1 time to create the very first set of matrixes in an iterated game, use update after that
-def initiate_matrixes(number_players):
+# initiate matrices should be run 1 time to create the very first set of matrices in an iterated game, use update after that
+def initiate_matrices(number_players):
     trust = np.full((number_players,number_players), 0.5)
     
     for player in range(number_players):
@@ -18,14 +18,14 @@ def initiate_matrixes(number_players):
     
     sus = 1 - trust
     
-    np.save('social_matrixes/trust_0.npy', trust)
+    np.save('social_matrices/trust_0.npy', trust)
 
 #this is done after each game
 def update_trust(run, trust_matrix):
     run += 1
     trust = trust_matrix
     
-    np.save(f'social_matrixes/trust_{run}.npy', trust)
+    np.save(f'social_matrices/trust_{run}.npy', trust)
 
 #this is done within the game and after the game to save the matrix
 def update_sus(run, game_finished, sus_matrix, person_1, person_2):
@@ -52,7 +52,7 @@ def update_sus(run, game_finished, sus_matrix, person_1, person_2):
     
     if game_finished:
         run += 1
-        np.save(f'social_matrixes/sus_{run}.npy', sus_matrix)
+        np.save(f'social_matrices/sus_{run}.npy', sus_matrix)
         return
     
     return sus
